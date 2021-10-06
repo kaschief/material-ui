88605ebcb13f6a88bf6768f582d0d7aee23223c4
Kaschief Johnson <kaschiefj@gmail.com>
2021-10-06 16:47:13 +0200

    components

--- material-ui-react-tutorial/src/App.js
+++ material-ui-react-tutorial/src/App.js
@@ -1,21 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 
 import { Favorite } from "@material-ui/icons";
+import { ButtonGroup } from "@material-ui/core";
+import { Checkbox } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
+
+const CheckBoxExample = () => {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <div>
+      <Checkbox
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+        color="primary"
+      />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          startIcon={<Favorite />}
-          variant="contained"
-          size="large"
-          color="secondary">
-          Hello World
-        </Button>
+        <TextField
+          variant="filled"
+          color="secondary"
+          type="date"
+          label="the time"></TextField>
+        <CheckBoxExample />
+        <ButtonGroup>
+          <Button
+            startIcon={<Favorite />}
+            variant="contained"
+            size="large"
+            color="secondary">
+            Hello World
+          </Button>
+        </ButtonGroup>
 
         <img src={logo} className="App-logo" alt="logo" />
       </header>
